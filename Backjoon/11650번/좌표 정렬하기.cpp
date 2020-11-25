b24c96912acd3db2ea0dea8863425c40c2b042a5
yuyw0712 <yuyw0712@naver.com>
2020-11-25 19:45:44 +0900

    11650번 좌표 정렬하기 풀이

---
+++ Backjoon/11650번/좌표 정렬하기.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <utility>
+#include <vector>
+using namespace std;
+int arr[100000][2];
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	vector <pair<int, int>> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i].first>>v[i].second;
+		
+	}
+
+	sort(v.begin(), v.end());
+	
+	for (int i = 0; i < n; i++) {
+		cout << v[i].first << " " << v[i].second;
+		cout << "\n";
+	}
+}
