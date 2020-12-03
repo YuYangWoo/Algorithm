a9c55daa07060fdef45c5c245d42b2d41d0207ce
yuyw0712 <yuyw0712@naver.com>
2020-12-03 16:05:26 +0900

    캥거루 세마리 풀이

---
+++ Backjoon/캥거루세마리/캥거루 세마리2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <string>
+#include <vector>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int a, b, c;
+
+
+
+	printf("%d", scanf_s("%d%d%d", &a, &b, &c));
+	
+
+	}
+
+
+
