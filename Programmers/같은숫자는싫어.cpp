c06ea2f44a3dd4fb743a5d47025a298e1d942070
yuyw0712 <yuyw0712@naver.com>
2021-01-01 03:47:45 +0900

    같은 숫자는 싫어 풀이

---
+++ Programmers/같은숫자는싫어.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+	vector<int> answer;
+
+	arr.erase(unique(arr.begin(), arr.end()), arr.end());
+	for (int i = 0; i < arr.size(); i++) {
+		answer.push_back(arr[i]);
+	}
+	return answer;
+}
+int main() {
+	vector<int> a{ 4,3 };
+	solution(a);
+}
