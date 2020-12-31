0b5c8172f9e2da1f5f5063d818e6fcd5f30fb412
yuyw0712 <yuyw0712@naver.com>
2021-01-01 04:32:41 +0900

    문자열 내 마음대로 정렬하기

---
+++ Programmers/문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector<string> solution(vector<string> strings, int n) {
+	vector<string> answer;
+	vector<char> cmp;
+	sort(strings.begin(), strings.end());
+	for (int i = 0; i < strings.size(); i++) {
+		cmp.push_back(strings[i].at(n));
+	}
+
+	//u e a
+	sort(cmp.begin(), cmp.end());
+	for (int i = 0; i < cmp.size(); i++) {
+		for (int j = 0; j < strings.size(); j++) {
+			if (cmp[i] == strings[j].at(n)) {
+				answer.push_back(strings[j]);
+				strings[j] = "####################################################################################################";
+				break;
+			}
+		}
+	}
+	return answer;
+}
+int main() {
+	vector<string> a{ "abce", "abcd", "cdx" };
+	solution(a,1);
+}
