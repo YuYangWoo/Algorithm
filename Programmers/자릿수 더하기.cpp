094087ba3083cd78e7ec894a0f8345dfe678d752
yuyw0712 <yuyw0712@naver.com>
2021-01-03 19:42:52 +0900

    약수의합 + 자릿수 더하기

---
+++ Programmers/자릿수 더하기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+int solution(int n)
+{
+	int answer = 0;
+	string cc = to_string(n);
+	for (int i = 0; i < cc.size(); i++) {
+	}
+
+	return answer;
+}
+int main() {
+	int a = 456;
+	string b = to_string(a);
+	int bb = b[1]-48;
+	cout << bb;
+}
