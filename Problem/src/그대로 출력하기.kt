3e0947c2aa18858140ef3c31d683f4969ffed157
yuyw0712 <yuyw0712@naver.com>
2021-10-30 23:32:11 +0900

    그대로 출력하기 풀이

---
+++ Problem/src/그대로 출력하기.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    while(scan.hasNext()) {
+        val input = scan.nextLine()
+        println(input)
+    }
+    scan.close()
+}
