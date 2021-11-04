6ce86d2b4f495c036672b9a55011d9d68213c664
yuyw0712 <yuyw0712@naver.com>
2021-11-04 21:42:36 +0900

    부분수열의 합 풀이

--- Problem/src/부분수열의 합.kt
+++ Problem/src/부분수열의 합.kt
@@ -1,18 +1,31 @@
 fun main() {
-    val (N,S) = readLine()!!.split(" ").map { it.toInt() }
+    val (N, S) = readLine()!!.split(" ").map { it.toInt() }
     val input = readLine()!!.split(" ").map { it.toInt() }
-    checkArrayyy = BooleanArray(N) {false}
-    dffs(input,S,0)
+    checkArrayyy = BooleanArray(N) { false }
+    countList = Array(N) { 0 }
+    for (i in 1..N) {
+        dffs(input, S, 0, i, 0)
+    }
+    print(countz)
 }
+
 var countz = 0
 lateinit var checkArrayyy: BooleanArray
-fun dffs(input: List<Int>, S: Int, sum: Int) {
-    if(sum == S) {
-        countz++
+lateinit var countList: Array<Int>
+
+fun dffs(input: List<Int>, S: Int, index: Int, pick: Int, start: Int) {
+    if (index == pick) {
+        if (countList.sum() == S) {
+            countz++
+        }
         return
     }
-    for(i in input.indices) {
-
+    for (i in start until input.size) {
+        if (checkArrayyy[i]) continue
+        countList[index] = input[i]
+        checkArrayyy[i] = true
+        dffs(input, S, index + 1, pick, i)
+        checkArrayyy[i] = false
     }
 
 }
