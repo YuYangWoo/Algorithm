c93b4408549372acb50f98eddc1ec67d9a2d047f
yuyw0712 <yuyw0712@naver.com>
2021-11-05 19:05:09 +0900

    전자레인지 풀이

--- Problem/src/전자레인지.kt
+++ Problem/src/전자레인지.kt
@@ -1,3 +1,26 @@
 fun main() {
+    val t = readLine()!!.toInt()
+    var arr = intArrayOf(300,60,10)
+    var sum = t
+    var cnt = intArrayOf(0,0,0)
+    while(sum >=10) {
+        if(sum>=300) {
+            cnt[0]++
+            sum-=300
+        }
+        else if(sum>=60) {
+            cnt[1]++
+            sum -= 60
+        }
+        else if(sum>=10) {
+            cnt[2]++
+            sum -= 10
+        }
 
+    }
+    if(sum == 0) {
+        cnt.forEach { print("${it} ") }
+    }  else {
+        print(-1)
+    }
 }
