615910bcb39c50e6e6ef7554cb3c07f128703cd3
yuyw0712 <yuyw0712@naver.com>
2021-11-05 18:54:54 +0900

    김인천씨의 식료품가게 풀이

--- Problem/src/김인천씨의 식료품가게.kt
+++ Problem/src/김인천씨의 식료품가게.kt
@@ -3,18 +3,60 @@ fun main() {
     for(i in 0 until t) {
         var N = readLine()!!.toInt()
         var list = readLine()!!.split(" ").map { it.toInt() }
-        var arr = ArrayList<Int>()
-        var answerList = ArrayList<Int>()
-        for(j in list.indices) {
-            arr.add((list[j]*0.75).toInt())
-        }
-        for(j in arr.indices) {
-            if(list.contains(arr[j])) {
-                answerList.add(arr[j])
+        aList = Array(N) {0}
+        inputChkList = BooleanArray(list.size+1) {false}
+        chkList = BooleanArray(list.size+1) {false}
+        answerListt = Array(N) {0}
+        ddffss(N,list,0,0,i)
+        print("Case #${i+1}: ")
+        answerListt.sort()
+        for(j in answerListt.indices) {
+            if(j == answerListt.size-1) {
+                println(answerListt[j])
+            }
+            else {
+                print("${answerListt[j]} ")
             }
         }
-        print("Case ${i+1}: ")
-        answerList.forEach { print("${it} ") }
-        println()
+
+    }
+
+}
+fun ddffss(N: Int, list: List<Int>,index: Int, start: Int, num: Int) {
+    if(index == N) {
+        var cnt = 0
+        for(i in aList.indices) {
+            for(j in list.indices) {
+                if(!inputChkList[j] && (aList[i]*0.75).toInt() == list[j]) {
+                    inputChkList[j] = true
+                    cnt++
+                    break
+                }
+            }
+        }
+        if(cnt == N) {
+            var z = 0
+           for(i in inputChkList.indices) {
+               if(inputChkList[i]) {
+                   answerListt[z] = list[i].toLong()
+                   z++
+               }
+           }
+            inputChkList = BooleanArray(list.size) {false}
+        }
+        inputChkList = BooleanArray(list.size) {false}
+
+        return
+    }
+    for(i in start until list.size) {
+        if(chkList[i]) continue
+        aList[index] = list[i].toLong()
+        chkList[i] = true
+        ddffss(N, list, index +1, i, num)
+        chkList[i] = false
     }
 }
+lateinit var answerListt: Array<Long>
+lateinit var aList: Array<Long>
+lateinit var inputChkList: BooleanArray
+lateinit var chkList: BooleanArray
